199b6af61edf875d89058e899aedaeee3924c2bf
jasson31 <jasson31@naver.com>
2020-10-07 21:53:53 +0900

    gitignore 추가 및 파이썬 가상 환경 변경

--- venv/Scripts/Activate.ps1
+++ venv/Scripts/Activate.ps1
@@ -27,7 +27,7 @@ function global:deactivate ([switch]$NonDestructive) {
 
 deactivate -nondestructive
 
-$env:VIRTUAL_ENV="C:\Users\jasson31\Documents\Projects\Pycharm\PandaStudy\venv"
+$env:VIRTUAL_ENV="C:\Users\jasson31\Documents\Projects\Pycharm\DPI-NetVisualizer\venv"
 
 if (! $env:VIRTUAL_ENV_DISABLE_PROMPT) {
     # Set the prompt to include the env name
